3e89d6248818ead9de4df32b15c83214020a55f1
Vayne <bernardobinna2@gmail.com>
2021-06-11 05:11:55 +0100

    done with faq mobile

--- src/components/landingFAQSection.vue
+++ src/components/landingFAQSection.vue
@@ -1,21 +1,41 @@
 <template>
-  <section class="row bg-faded-blue content pb-0">
+  <section class="row bg-faded-blue content">
     <div>
       <h6 class="text-center mb-0">FAQs</h6>
       <h2 class="sect-header w-lg-50">Let’s answer your questions</h2>
       <div class="accordion" id="faqAccordion">
-        <div class="question" v-for="(faq, index) in faqs" :key="index" :class="( (index + 1) === faqs.length) ? 'mb-0' : ''">
-          <h5 id="`question-header-${index}`"  data-bs-toggle="collapse" :data-bs-target="`#question-${index}`" aria-expanded="false" :aria-controls="`#question-${index}`">
-            {{ faq.question }}
+        <div
+          class="question"
+          v-for="(faq, index) in faqs"
+          :key="index"
+          :class="index + 1 === faqs.length ? 'mb-0' : ''"
+        >
+          <div
+            class="d-flex"
+            id="`question-header-${index}`"
+            data-bs-toggle="collapse"
+            :data-bs-target="`#question-${index}`"
+            aria-expanded="false"
+            :aria-controls="`#question-${index}`"
+          >
+            <h5>
+              {{ faq.question }}
+            </h5>
             <img
-                src="@/assets/images/landing/plus-circle.svg"
-                class="expand-icon"
-                alt="Expand question icon"
+              src="@/assets/images/landing/plus-circle.svg"
+              class="expand-icon"
+              alt="Expand question icon"
             />
-          </h5>
-          <div :id="`question-${index}`" class="accordion-collapse collapse" :aria-labelledby="`#question-header-${index}`" data-bs-parent="#faqAccordion">
+          </div>
+
+          <div
+            :id="`question-${index}`"
+            class="accordion-collapse collapse"
+            :aria-labelledby="`#question-header-${index}`"
+            data-bs-parent="#faqAccordion"
+          >
             <div class="accordion-body px-0">
-              <strong>{{faq.answer}}</strong>
+              <strong>{{ faq.answer }}</strong>
             </div>
           </div>
         </div>
@@ -24,10 +44,10 @@
   </section>
 </template>
 <script>
-import {reactive} from '@vue/composition-api'
+import { reactive } from '@vue/composition-api'
 export default {
-  name: "LandingFAQSection",
-  setup(){
+  name: 'LandingFAQSection',
+  setup() {
     const data = reactive({
       faqs: [
         {
@@ -53,33 +73,35 @@ export default {
         {
           question: 'How long till I recieve my money?',
           answer: 'Working on this'
-        },
-
+        }
       ]
     })
 
-    return {...data}
-  },
-};
+    return { ...data }
+  }
+}
 </script>
 <style lang="scss" scoped>
 @import 'src/assets/scss/core/mixins';
 @import 'src/assets/scss/core/variables';
 
 .content {
-  padding-bottom: toRem(143px);
+  padding-bottom: 0 !important;
   @include screen('med') {
-    padding-bottom: toRem(80px);
+    padding-top: toRem(64px);
   }
 }
 
-.expand-icon{
+.expand-icon {
   width: toRem(24px);
   height: toRem(24px);
 }
 
-.accordion{
+.accordion {
   margin-bottom: toRem(206px);
+  @include screen('med') {
+    margin-bottom: toRem(96px);
+  }
 }
 
 .question {
@@ -92,12 +114,12 @@ export default {
   font-size: toRem(20px);
   line-height: toRem(32px);
 
-  img{
-    float: right;
+  img {
+    margin-left: auto;
   }
 }
 
-.sect-header{
+.sect-header {
   margin-top: toRem(4px);
   margin-bottom: toRem(64px);
   max-width: toRem(504px);
@@ -110,5 +132,10 @@ h2 {
     line-height: toRem(30px);
   }
 }
-</style>
 
+h6 {
+  @include screen('med') {
+    display: none;
+  }
+}
+</style>
