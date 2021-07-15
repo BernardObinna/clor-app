252e413e508e48030005db8bd8de17e5447e54f1
Vayne <bernardobinna2@gmail.com>
2021-07-15 20:27:04 +0100

    added axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const axiosInstance = {
   install(Vue) {
     Vue.prototype.$axios = axios.create({
-      baseURL: 'http://localhost:8000/api/',
+      baseURL: 'http://34.123.40.176/v1/',
       headers: {
         Authorization: 'Bearer {token}'
       }
